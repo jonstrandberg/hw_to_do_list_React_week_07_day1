381a360d69601efd733898fc447550d307b3c4f6
Jonathan Strandberg <jonstrandberg7@gmail.com>
2023-01-16 19:36:11 +0000

    everything except colour coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  
+  const [items, setItems] = useState([
+    {id: 1, name: "Buy shopping"},
+    {id: 2, name: "Clean bathroom"},
+    {id: 3, name: "Pay CodeClan"},
+    {id: 4, name: "Take dog for walk"}
+  ])
+
+  const [newItem, setNewItem] = useState("")
+
+  const [priority, setPriority] = useState()
+
+  const handleItemInput = (evt) => {
+    setNewItem (evt.target.value)
+  } 
+
+  const saveNewItemWithPriority = (evt) => {
+    evt.preventDefault()
+    const newItemObject = {id: Date.now(), name: newItem, priority: priority}
+    const newListOfItems = [...items, newItemObject]
+    setItems(newListOfItems)
+    setNewItem("")
+    setPriority("")
+  }
+
+  const completedItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id)
+    setItems(newItems)
+  }
+
+  const handlePriorityChange = (evt) => {
+    setPriority(evt.target.value);
+  }
+  
+  const toDoList = items.map((item) => {
+    return ( 
+    <li key={(item.id)}>
+      {item.name}
+      <button onClick={() => {completedItem(item.id)}}>Completed</button>
+    </li>
+  )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> To Do List </h1>
+
+    <hr></hr>
+
+    <ul>
+      {toDoList}
+    </ul>
+
+    <form>
+    <fieldset>
+    <legend>Select priority</legend>
+
+    <div>
+      <input type="radio" id="low-priority" name="priority" value="low"
+          checked={priority === "low"}
+          onChange={handlePriorityChange}
+      />
+      <label htmlFor="low-priority">Low Priority</label>
     </div>
-  );
+
+    <div>
+      <input type="radio" id="high-priority" name="priority" value="high"
+          checked={priority === "high"}
+          onChange={handlePriorityChange}
+      />
+      <label htmlFor="high-priority">High Priority</label>
+    </div>
+  </fieldset>
+  </form>
+
+  <form onSubmit={saveNewItemWithPriority}>
+      <label htmlFor="new-item"> Add new Item</label>
+      <input id="new-item" type="text" value={newItem} onChange={handleItemInput}></input>
+      <input type="submit" value="Save New Item" ></input>
+    </form>
+
+  </div>
+  )
 }
 
 export default App;
